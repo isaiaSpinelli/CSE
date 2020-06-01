febf31fb2e763ca271e6f3658f877328d84f5df5
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2020-06-01 10:30:57 +0200

    Refactor

--- repo_test/main2.c
+++ repo_test/main2.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  /* prend une photo avec la Pi Cam */
-  printf("Prend une photo...\n");
-  system("python takePhoto.py");
-  printf("Photo ok !\n");  
+	/* prend une photo avec la Pi Cam */
+	printf("Prend une photo...\n");
+	system("python takePhoto.py");
+	printf("Photo ok !\n");  
 
- /* Execute le script python afin de prendre la photo, reconnaitre le texte et stocker dans un fichier */
-  printf("\nLecture des caracteres...\n"); 
-  system("python ocr_arg.py picture.jpg");
+	/* Execute le script python afin d’appliquer l'algorithme OCR 
+	    et stocker le texte lu dans un fichier (res.txt) */
+	printf("\nLecture des caracteres...\n"); 
+	system("python ocr_arg.py picture.jpg");
 
-  /* Parse le texte reconnu avec l'algo ocr dans un fichier res.xml */  
- // printf("\nParsing... \n");
- // system("python parse_res.py");
- // printf("Fichier res.xml OK\n");
+	/* Parse le texte lu avec l'algo ocr (res.txt) dans un fichier res.xml */  
+	printf("\nParsing... \n");
+	system("python parse_res.py");
+	printf("Fichier res.xml OK\n");
 
 
-  printf("\nFin\n");
+	printf("\nFin\n");
 
-  return 0;
+	return 0;
 }
--- repo_test/ocr_arg.py
+++ repo_test/ocr_arg.py
@@ -4,22 +4,24 @@ import pytesseract
 import os
 import sys
 
-
+# Chemin de l'image de base
 fn = "image_sans_bord_grayscale.PNG"
-# Recupere le chemin de l'image
 
+# Recupere le chemin de l'image en argument
 if len(sys.argv) > 1:
     fn = sys.argv[1]
 else:
     print("Warning ! : need arg (par defaut take : image_sans_bord_grayscale.PNG\r\n")
+	
 # recupere l'image
 img_cv = cv2.imread(fn)
 
-# transforme l'image en rgb
+# transforme l'image en image rgb
 img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
 
 # Lis les caracteres
 texte = pytesseract.image_to_string(img_rgb)
+# Affiche le resutlat
 print("ALGO OCR RESULTAT :\n")
 print("--------------------------\n");
 print(texte)
